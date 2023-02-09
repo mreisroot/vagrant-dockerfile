112a0d818a0da3eb8e19821f40d6615935ca269a
Miguel Reis <miguel_reis08@protonmail.com>
2023-02-09 09:47:04 -0300

    Add provisioner script

---
+++ provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Adicionar o repositório do Docker
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt update
+
+# Instalar o Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Criar imagem de container Docker a partir do Dockerfile
+sudo docker image build -t viacep .
+
+# Criar e executar container da aplicação VIACEP
+sudo docker container run --name viacep-docker -dp 80:80 viacep
