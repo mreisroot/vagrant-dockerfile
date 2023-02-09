2cbac41f1e1280f5ef780e41fbe9aad0edce5a27
Miguel Reis <miguel_reis08@protonmail.com>
2023-02-09 17:11:34 -0300

    Update provisioner script

--- provision.sh
+++ provision.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# Adicionar o repositório do Docker
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
+# Executar o script inteiro como root
 
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo su - << EOF
 
-echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # Adicionar o repositório do Docker
+  apt update
+  apt install -y ca-certificates curl gnupg lsb-release
+  
+  mkdir -m 0755 -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  
+  echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  chmod a+r /etc/apt/keyrings/docker.gpg
+  apt update
+  
+  # Instalar o Docker
+  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  
+  # Criar imagem de container Docker a partir do Dockerfile
+  docker image build -t viacep .
+  
+  # Criar e executar container da aplicação VIACEP
+  docker container run --name viacep-docker -dp 80:80 viacep
 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt update
-
-# Instalar o Docker
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Criar imagem de container Docker a partir do Dockerfile
-sudo docker image build -t viacep .
-
-# Criar e executar container da aplicação VIACEP
-sudo docker container run --name viacep-docker -dp 80:80 viacep
+EOF
